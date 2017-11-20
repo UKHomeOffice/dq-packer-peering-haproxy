985f568db3f7c49d0ace8b30c59d3b709e55c4b6
david-wirelab <info@wirelab.co.uk>
2017-11-20 09:38:49 +0000

    bash script update

--- templates/gets3content.sh
+++ templates/gets3content.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 aws s3 cp s3://globalbucketname/haproxy.cfg /etc/haproxy/haproxy.cfg --region eu-west-2
-service haproxy reload
+haproxy -f /etc/haproxy/haproxy.cfg -p /var/run/haproxy.pid -st $(cat /var/run/haproxy.pid)
