edc13cc6196a9efb268d2c703e43b14d62d28103
Chris Nesbitt-Smith <chris@cns.me.uk>
2017-12-21 14:47:29 +0000

    get the name of the bucket from userdata

--- templates/gets3content.sh
+++ templates/gets3content.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-aws s3 cp s3://globalbucketname/haproxy.cfg /etc/haproxy/haproxy.cfg --region eu-west-2
+aws s3 cp s3://$(curl 169.254.169.254/latest/user-data)/haproxy.cfg /etc/haproxy/haproxy.cfg --region eu-west-2
 haproxy -f /etc/haproxy/haproxy.cfg -p /var/run/haproxy.pid -st $(cat /var/run/haproxy.pid)
