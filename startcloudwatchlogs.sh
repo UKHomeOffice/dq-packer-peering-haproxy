d4fd3e052374b1067a28d71df634e1c8f6c7ef63
david-wirelab <info@wirelab.co.uk>
2018-05-15 16:46:19 +0100

    fix startup script

--- startcloudwatchlogs.sh
+++ startcloudwatchlogs.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 # Get the instance region and inject it in the conf
-REGION=$(ec2metadata --availability-zone | head -c-2)
+EC2_AVAIL_ZONE=`curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone`
+EC2_REGION="`echo \"$EC2_AVAIL_ZONE\" | sed 's/[a-z]$//'`"
 sed -i -e 's/.*region.*/region = '$REGION'/' /var/awslogs/etc/aws.conf
 
 # Restart the awslogs agent
-service awslogs restart
+systemctl restart awslogs
