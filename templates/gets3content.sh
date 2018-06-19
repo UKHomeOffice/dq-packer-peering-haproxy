3fe301269960073291129b84fb33a109dbaaf543
david-wirelab <info@wirelab.co.uk>
2018-06-19 12:42:08 +0100

    change haproxy reload flag

--- templates/gets3content.sh
+++ templates/gets3content.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 aws s3 cp s3://$(curl 169.254.169.254/latest/user-data)/haproxy.cfg /etc/haproxy/haproxy.cfg --region eu-west-2
 /etc/ssl/certs/make-dummy-cert /etc/ssl/certs/self-signed-cert
-sudo haproxy -f /etc/haproxy/haproxy.cfg -p /var/run/haproxy.pid -st $(cat /var/run/haproxy.pid)
+sudo haproxy -f /etc/haproxy/haproxy.cfg -p /var/run/haproxy.pid -sf $(cat /var/run/haproxy.pid)
